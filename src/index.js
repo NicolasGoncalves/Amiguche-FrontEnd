b9b7e0520043e94bf6a8849eb68fbd3ee7a97408
NicolasGoncalves <nicolasbabilas11@gmail.com>
2025-04-25 11:04:30 -0300

    Criação de um segundo repositorio + Alterações na LandingPage

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+
+import './index.css';
+
+import App from './App';
+import LandingPage from './pages/LandingPage';
+import Login from './pages/Login';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="*" element={<App />} />
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </BrowserRouter>
+
+);
+
+
