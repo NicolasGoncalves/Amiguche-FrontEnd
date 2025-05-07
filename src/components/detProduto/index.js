9033dea29bcaffef4bbdc6aeecaec2b33ffc99f4
NicolasGoncalves <nicolasbabilas11@gmail.com>
2025-05-06 23:20:09 -0300

    Componente de descrição do produto

---
+++ src/components/detProduto/index.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./index.scss";
+
+export default function DetProduto(props) {
+  const [img, setImage] = useState(<h1>Foto do Amigurumi</h1>);
+
+  useEffect(() => {
+    if (props.img !== undefined) setImage(<img src={props.img} alt="" />);
+  }, [props.img]);
+
+  return (
+    <main>
+      <section className="detalhes-produto">
+        <div className="sec1">
+          <div>
+            <Link>
+              <h2>Voltar</h2>
+            </Link>
+            <h1>Detalhes do Produto</h1>
+          </div>
+          <hr />
+        </div>
+
+        <div className="sec2">
+          <div className="sec2-img">
+            <div>{img}</div>
+
+            <button>
+              <img src="./images/UploadIcon.png" alt="" />
+              Fazer Upload
+            </button>
+          </div>
+
+          <div className="sec2-info">
+            <div className="sec2-info-input">
+              <label id="nome">Nome: </label>
+              <input id="nome" type="text" />
+            </div>
+
+            <div className="sec2-info-input">
+              <label id="preco">Preço: </label>
+              <input id="preco" type="number" />
+            </div>
+            <div className="sec2-info-area">
+              <label id="desc">Descrição: </label>
+              <textarea id="desc" cols="30" rows="10"></textarea>
+            </div>
+          </div>
+        </div>
+
+        <div className="sec3">
+          <button>Salvar</button>
+          <button>Excluir</button>
+        </div>
+      </section>
+    </main>
+  );
+}
