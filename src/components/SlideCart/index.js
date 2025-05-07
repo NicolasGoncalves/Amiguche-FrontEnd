9b7d73f5b2d2a4f5b420cba44d1779f5fa854846
pudimpudi <damatodutragabrielle@gmail.com>
2025-05-07 09:14:08 -0300

    scss slidecart atualizado

--- src/components/SlideCart/index.js
+++ src/components/SlideCart/index.js
@@ -5,11 +5,20 @@ export default function SlideCart({ isOpen, onClose }) {
     return (
         <div className={`slide-cart ${isOpen ? "open" : ""}`}>
             <button className="close-btn" onClick={onClose}>✖</button>
-            <h2>Seu Carrinho</h2>
-            <ul>
-                {/* Itens do carrinho aqui */}
-            </ul>
-            <button className="checkout-btn">Finalizar Compra</button>
+            <h2>Seu Carrinho 🛒</h2>
+            <hr></hr>
+
+            <div className="box-produtos">
+                {/*box para cada produto adicionado*/}
+                <h3>Produto 1</h3>
+            </div>
+
+            <h3>Valor Total: </h3>
+
+            <div className="btn-container">
+                <button className="checkout-btn">Finalizar Compra</button>
+            </div>
+            
         </div>
     );
 }
